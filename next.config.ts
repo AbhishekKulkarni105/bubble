64a93ca4f5ca3940971e4900e471de10e07ffb59
Diamondback Migration <dev@diamondback.local>
2026-07-03 13:26:01 +0530

    Change next config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-  },
+  typedRoutes: true, // Moved to the top level
 };
 
 export default nextConfig;
